023d648f57aeaeb2708baba597ddf002ef41748c
DESKTOP-HTTNB87\NOY <a.ptm1711@gmail.com>
2022-10-02 20:57:08 +0700

    ep7 - have 2 bug ( edit , check )

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -25,7 +25,6 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         alert;
-
-
     }
+    
     ?>
--- admin/inc/scripts.php
+++ admin/inc/scripts.php
@@ -1,2 +1,16 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+<script>
+    function alert(type,msg){
+        let bs_class = (type == 'success') ? 'alert-success' : 'alert-danger';
+        let element = document.createElement('div');
+        element.innerHTML = `
+            <div class="alert ${bs_class} alert-dismissible fade show custom-alert" role="alert">
+                <strong class = "me-3">${msg}</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `;
+        document.body.append(element);
+    }
+</script>
